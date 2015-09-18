f8f42d5e22dc90e9258502e286444df3a7518e99
pkzxs <pkzxs123@gmail.com>
2015-09-18 17:50:57 +0800

    first repo

---
+++ com.aurora.aumusic/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace com.aurora.aumusic
+{
+    public partial class MainPage:Page
+    {
+
+    }
+}
---
+++ com.aurora.aumusic/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace com.aurora.aumusic
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Window.Current.SetTitleBar(Titlepanel);
+            MainFrame.Navigate(typeof(com.aurora.aumusic.MymusicPage));
+        }
+
+        private void Menubtn_Click(object sender, RoutedEventArgs e)
+        {
+            Menudrawer.IsPaneOpen = !Menudrawer.IsPaneOpen;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int x = MenuList.SelectedIndex;
+            switch(x)
+            {
+                case 0: MainFrame.Navigate(typeof(com.aurora.aumusic.MymusicPage)); break;
+                case 1: MainFrame.Navigate(typeof(NowPage)); break;
+                case 2: MainFrame.Navigate(typeof(ListPage)); break;
+                case 3: MainFrame.Navigate(typeof(SettingsPage)); break;
+
+            }
+        }
+    }
+}
