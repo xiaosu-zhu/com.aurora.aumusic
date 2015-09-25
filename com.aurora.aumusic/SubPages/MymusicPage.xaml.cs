790ddbdc6a310cb48df6c6b5306a265a18c5a9c8
pkzxs <pkzxs123@gmail.com>
2015-09-25 10:23:15 +0800

    更正注释

--- com.aurora.aumusic/SubPages/MymusicPage.xaml.cs
+++ com.aurora.aumusic/SubPages/MymusicPage.xaml.cs
@@ -28,34 +28,34 @@ namespace com.aurora.aumusic
         {
             //横向瀑布流
 
-            //三组流宽度记录
+            //三组流长度记录
             KeyValuePair<double, int>[] flowLength = new KeyValuePair<double, int>[3];
             foreach(int index in Enumerable.Range(0,3))
             {
                 flowLength[index] = new KeyValuePair<double, int>(0.0, index);
             }
 
-            //每组宽度为总宽度1/3
+            //每组长度为总长度1/3
             double flowWidth = availableSize.Width / 3;
 
-            //子控件高为组高，宽无限制
+            //子控件宽为组宽，长无限制
             Size childMeasureSize = new Size(flowWidth, double.PositiveInfinity);
 
-            //子控件遍历计算宽度
+            //子控件遍历计算长度
             foreach(UIElement childElement in Children)
             {
                 childElement.Measure(childMeasureSize);
                 Size childSize = childElement.DesiredSize;
-                //得到子控件宽
+                //得到子控件长
                 double childLength = childSize.Height;
-                //暂存最短流宽度
+                //暂存最短流长度
                 var tempPair = flowLength[0];
-                //最短流宽度重新计算
+                //最短流长度重新计算
                 flowLength[0] = new KeyValuePair<double, int>(tempPair.Key + childLength, tempPair.Value);
-                //重新按流宽度排列键值对        这里以Key 的值作为排列依据，flowWidth[0]为Key最小的键值对，P可替换为任意字母
+                //重新按流长度排列键值对        这里以Key 的值作为排列依据，flowWidth[0]为Key最小的键值对，P可替换为任意字母
                 flowLength = flowLength.OrderBy(P => P.Key).ToArray();
             }
-            //返回 宽：最长流的宽；高：传入的高
+            //返回 长：最长流的长；宽：传入的宽
             return new Size(availableSize.Width, flowLength.Last().Key);
         }
 
@@ -77,7 +77,7 @@ namespace com.aurora.aumusic
                 Size elemSize = childElem.DesiredSize;
                 double elemLength = elemSize.Height;
 
-                //得到最短流宽度
+                //得到最短流长度
                 var pair = flowLength[0];
                 double chosenFlowHeight = pair.Key;
                 int chosenFlowIdx = pair.Value;
@@ -85,7 +85,7 @@ namespace com.aurora.aumusic
                 // 设置即将放置的控件坐标
                 Point pt = new Point(xWidth[chosenFlowIdx], chosenFlowHeight);
 
-                // 调用Arrange进行子控件布局。并让子控件利用上整个流的宽度。
+                // 调用Arrange进行子控件布局。并让子控件利用上整个流的长度。
                 childElem.Arrange(new Rect(pt, new Size(flowWidth, elemSize.Height)));
 
                 // 重新计算最短流。
