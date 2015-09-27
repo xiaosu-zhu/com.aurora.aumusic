46b5e54efccf7dd735ea423bfba53b1e43cfc439
pkzxs <pkzxs123@gmail.com>
2015-09-27 11:51:36 +0800

    .

--- com.aurora.aumusic/SubPages/NowPage.xaml.cs
+++ com.aurora.aumusic/SubPages/NowPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
+
 
 namespace com.aurora.aumusic
 {
