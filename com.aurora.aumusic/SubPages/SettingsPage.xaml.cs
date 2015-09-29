966bfcb76f8bde549f73ecbd15bd103ee01e9dd8
pkzxs <pkzxs123@gmail.com>
2015-09-29 17:15:14 +0800

    完成文件夹选取，文件夹列表和所有音乐文件遍历稍后完成

--- com.aurora.aumusic/SubPages/SettingsPage.xaml.cs
+++ com.aurora.aumusic/SubPages/SettingsPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 using Windows.UI.Xaml.Controls;
 
 namespace com.aurora.aumusic
@@ -12,6 +13,29 @@ namespace com.aurora.aumusic
         public SettingsPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+            folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add(".mp3");
+            folderPicker.FileTypeFilter.Add(".m4a");
+            folderPicker.FileTypeFilter.Add(".flac");
+            folderPicker.FileTypeFilter.Add(".aac");
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                // Application now has read/write access to all contents in the picked folder
+                // (including other sub-folder contents)
+                Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.AddOrReplace("MusicFolderToken", folder);
+
+            }
+            else
+            {
+
+            }
+
         }
     }
 }
