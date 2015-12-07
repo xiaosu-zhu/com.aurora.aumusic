d92807e10d3bc849980236723140ee8d6a3c9d61
pkzxs <pkzxs123@gmail.com>
2015-12-07 08:45:44 +0800

    add MIT license

--- com.aurora.aumusic/SubPages/AlbumFlowPage.xaml.cs
+++ com.aurora.aumusic/SubPages/AlbumFlowPage.xaml.cs
@@ -1,4 +1,19 @@
-﻿using System.Linq;
+﻿//Copyright(C) 2015 Aurora Studio
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+//to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+//and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+//WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+/// <summary>
+/// Usings
+/// </summary>
+using System.Linq;
 using Windows.Media.Playback;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -25,6 +40,9 @@ using Windows.Storage;
 
 namespace com.aurora.aumusic
 {
+    /// <summary>
+    /// This is the proccessing code of the AlbumFlowpage
+    /// </summary>
     public sealed partial class AlbumFlowPage : Page
     {
         CurrentTheme Theme = ((Window.Current.Content as Frame).Content as MainPage).Theme;
@@ -76,6 +94,11 @@ namespace com.aurora.aumusic
             if (str == null)
             {
                 ((Window.Current.Content as Frame).Content as MainPage).FirstCreate();
+            }
+            str = ApplicationSettingsHelper.ReadResetSettingsValue("NewAdded");
+            if(str != null)
+            {
+                //notify to refresh
             }
             AlbumFlowZoom.IsZoomedInViewActive = false;
         }
